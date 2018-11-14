98188a624f571e224a62ec553fb3458f376baaeb
mmheller <mm_heller@yahoo.com>
2018-11-14 11:56:24 -0700

    updates for options 7

--- mdEditorFileInjector_v1.py
+++ mdEditorFileInjector_v1.py
@@ -77,7 +77,6 @@ def Process_mdEditorInsert(strFileNamePath, strElement2Edit, json_mdJSON2Inject,
           arrayFinalContent = []
           ii = 0
           for mdEditorItem in dic_mdJSON["data"]:
-            ii+=1
             blnIsContactItem = False
             if (blnArgContactItem == True):
                 blnAddTheOriginalContact = True
@@ -85,6 +84,19 @@ def Process_mdEditorInsert(strFileNamePath, strElement2Edit, json_mdJSON2Inject,
             if ("type" in mdEditorItem):
                 if (mdEditorItem["type"] == "contacts"):
                     blnIsContactItem = True
+
+            blnIsSettingsItem = False
+            if ("type" in mdEditorItem):
+                if (mdEditorItem["type"] == "settings"):
+                    blnIsSettingsItem = True
+
+            if (strUpdateOption == "7") & (blnIsContactItem == False) & (blnIsSettingsItem == False):
+                     strSBID = Get_mdEditorValue(mdEditorItem, "metadata|resourceInfo|citation|identifier" , "gov.sciencebase.catalog")
+                     if (strSBID == ""):
+                         strSBID = Get_mdEditorValue(mdEditorItem, "metadata|metadataInfo|metadataIdentifier" , "gov.sciencebase.catalog")
+                     json_mdJSON2Inject = BuildOption7JSON(strSBID)
+                     if (strSBID == ""):
+                        print "cannot find Sciencebase id"
 
             blnIsContactProjectProduct = False
             if ("json" in  mdEditorItem["attributes"]):#determine if a Contact, Project, or Product element
@@ -131,6 +143,8 @@ def Process_mdEditorInsert(strFileNamePath, strElement2Edit, json_mdJSON2Inject,
                     mdEditorItem["attributes"]["json"] = json.dumps(json_mdJSON, indent=4)  #json_mdJSONTMP = json_mdJSON
 
             elif (blnIsContactItem == False) & (blnIsContactItem == blnArgContactItem) & (blnIsContactProjectProduct == True) & (strUpdateOption != 3):                        #if contact argument FALSE and  passed equals item yes/no type
+                 ii+=1
+
                  print "*****************************************************************************************************************"
                  json_mdJSON = json.loads(mdEditorItem["attributes"]["json"])  #grab the json within the product/project
                  arrayElement2Edit = strElement2Edit.split("|")  # convert the path of the element to edit to array
@@ -162,9 +176,12 @@ def Process_mdEditorInsert(strFileNamePath, strElement2Edit, json_mdJSON2Inject,
                                     strpPossibleFineGrainItemExistence = json.dumps(pPossibleFineGrainItemExistence)
                                     strjson_FindContent = json.dumps(json_FindContent).replace("{","").replace("}","")
 
-                                    json_mdJSONTMP.remove(pPossibleFineGrainItemExistence)  # for this find/replace scenario remove the original that matches the find
+                                    print strpPossibleFineGrainItemExistence
+                                    print strjson_FindContent
 
                                     if (strpPossibleFineGrainItemExistence.find(strjson_FindContent) >= 0):
+                                        json_mdJSONTMP.remove(pPossibleFineGrainItemExistence)  # for this find/replace scenario remove the original that matches the find
+
                                         blnFindTextFound = True  
                                         strjson_mdJSON2Inject = json.dumps(json_mdJSON2Inject)
 
@@ -174,10 +191,12 @@ def Process_mdEditorInsert(strFileNamePath, strElement2Edit, json_mdJSON2Inject,
                                         strpPossibleFineGrainItemExistence = strpPossibleFineGrainItemExistence.replace(strjson_FindContent, strjson_FindContent +', ' + strjson_mdJSON2Inject)
 
                                         json_mdJSON2Inject2 = json.loads(strpPossibleFineGrainItemExistence)
+                                        json_mdJSONTMP.append(json_mdJSON2Inject2)
                             if (not (blnFindTextFound)) and (strUpdateOption == "1") and (json_mdJSON2Inject <> "") and (json_FindContent <> ""):
                                 blnAddContent = False
                             if (blnAddContent) and (json_mdJSON2Inject <> "") and (strUpdateOption == "6"):
-                                json_mdJSONTMP.append(json_mdJSON2Inject2)
+                                #json_mdJSONTMP.append(json_mdJSON2Inject2)
+                                print "already appended"
                             elif (blnAddContent) and (json_mdJSON2Inject <> ""):
                                 json_mdJSONTMP.append(json_mdJSON2Inject)
                         else:
@@ -213,10 +232,85 @@ def Process_mdEditorInsert(strFileNamePath, strElement2Edit, json_mdJSON2Inject,
       print e.message
       pass
 
+
+def Get_mdEditorValue(mdEditorItem, strElement2Edit, json_FindContent):
+  try:
+            strSBID = ""
+            blnIsContactItem = False
+            if ("type" in mdEditorItem):
+                if (mdEditorItem["type"] == "contacts"):
+                    blnIsContactItem = True
+
+            blnIsContactProjectProduct = False
+            if ("json" in  mdEditorItem["attributes"]):#determine if a Contact, Project, or Product element
+                blnIsContactProjectProduct = True
+
+            if (blnIsContactItem == False) & (blnIsContactProjectProduct == True):                        #if contact argument FALSE and  passed equals item yes/no type
+                 print "*****************************************************************************************************************"
+                 json_mdJSON = json.loads(mdEditorItem["attributes"]["json"])  #grab the json within the product/project
+                 arrayElement2Edit = strElement2Edit.split("|")  # convert the path of the element to edit to array
+                 iE = 0
+
+                 json_mdJSONTMP = json_mdJSON  #probably don't need to set this tmp var
+
+                 for JSONElement in arrayElement2Edit:
+                    print "looking for element:" + JSONElement
+                    iE += 1
+
+                    if (JSONElement in json_mdJSONTMP):
+                        json_mdJSONTMP = json_mdJSONTMP[JSONElement]
+
+                        if(iE == len(arrayElement2Edit)):
+                            if (strElement2Edit == "metadata|metadataInfo|metadataIdentifier"):
+                                if (str(json_mdJSONTMP.get("namespace")) == "gov.sciencebase.catalog"):
+                                    strSBID = str(json_mdJSONTMP.get("identifier"))
+                                    return strSBID
+                            else:
+                                for pPossibleFineGrainItemExistence in json_mdJSONTMP:
+                                    pFineGrainItemValue = pPossibleFineGrainItemExistence.get("namespace")
+                                    if (str(pFineGrainItemValue) == "gov.sciencebase.catalog"):
+                                        strSBID = str(pPossibleFineGrainItemExistence.get("identifier"))
+                                        return strSBID
+                        else:
+                            print ""
+
+            print "Sciencebase ID found " + strSBID
+            return strSBID
+
+  except Exception, e:
+        import traceback, sys# If an error occurred, print line number and error message
+        tb = sys.exc_info()[2]
+        print "Get_mdEditorValue: Line %i" % tb.tb_lineno
+        print e.message
+        pass
+
 def CheckInjectString(str_mdJSON2InjectFile):
   try:
       with open(str_mdJSON2InjectFile) as json_data:  #read the mdEditor file
           dic_mdJSON = json.load(json_data)
+
+      return dic_mdJSON
+  except Exception, e:
+      import traceback, sys# If an error occurred, print line number and error message
+      tb = sys.exc_info()[2]
+      print "CheckInjectString: Line %i" % tb.tb_lineno
+      print e.message
+      return ""
+
+def BuildOption7JSON(strSBID):
+  try:
+      strSBURL = "https://www.sciencebase.gov/catalog/item/" + strSBID
+      strInjectContent = '{"distributor": ['
+      strInjectContent += '{"contact": { "role": "pointOfContact",'
+      strInjectContent += '"party": [{"contactId": "edc6b779-3352-4a81-8430-76bcce1bfcb3"}]},'
+      strInjectContent += '"transferOption": [{'
+      strInjectContent += '"onlineOption": [{"uri":"'
+      strInjectContent += strSBURL
+      strInjectContent += '",'
+      strInjectContent += '"name": "Product Web-page with Downloadable Files"'
+      strInjectContent += '}]}]}]}'
+
+      dic_mdJSON = json.loads(strInjectContent)
 
       return dic_mdJSON
   except Exception, e:
@@ -253,6 +347,12 @@ try:
 
                 if (pRoot <> strNewFolder): #don't process the files that are processed during this session
                     print "processing file:" + strFileNamePath
+
+                    if (strUpdateOption == "7"):
+                        strElement2Edit = "metadata|resourceDistribution"
+                        blnProcessContacts = False
+                        json_FindContent = ""
+
                     strOutputText = Process_mdEditorInsert(strFileNamePath, strElement2Edit, json_InjectContent, blnProcessContacts, json_FindContent, strUpdateOption)
                     strOutputFileNamePath2 = CreateBlankASCIIFile(strNewFolder + '\\' + pFile)
                     with open(strOutputFileNamePath2, mode="wb") as outfile:
